5c706c88211c98397f1c48b28166cfb7b8f32b54
internessi <50959344+internessi@users.noreply.github.com>
2021-07-14 20:32:59 +0200

    asd

--- lib/Pages/Home/Timers/timerSimple.dart
+++ lib/Pages/Home/Timers/timerSimple.dart
@@ -19,6 +19,7 @@ class _TimerSimpleState extends State<TimerSimple>
 
   List timerTypeColor = [timerColorFight,  timerColorPause, timerColorPrep];
   List timerTypeColorBg = [timerColorFightBg, timerColorPauseBg,  timerColorPrepBg];
+  List timerTypeColorR = [timerColorFightR, timerColorPauseR,  timerColorPrepR];
   List timerTypeText = ['Aktivzeit', 'Erholungszeit',  'Vorbereitung'];
 
   String get timerString {
@@ -86,38 +87,72 @@ class _TimerSimpleState extends State<TimerSimple>
                   style: TextStyle(fontWeight: FontWeight.normal),
                 ),
                 actions: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child:
-                    Container(
-                        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
-                        child: RichText(
-                          text: TextSpan(
-                            style: TextStyle(color: greyColor, fontWeight: FontWeight.normal),
-                            children: [
-                              WidgetSpan(
-                                child: Icon(Icons.arrow_forward, color: greyColor, size: 18),
-                              ),
-                              TextSpan(
-                                text: ' Trainer anzeigen',
-                              ),
-                            ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          child: Text(
+                              "0:05".toUpperCase(),
+                              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)
                           ),
-                        )
-
-                    ),
-                  ),
-                  IconButton(
-                      icon: Icon(
-                        Icons.fitness_center,
-                        color: greyColor,
-                        size: 18,
+                          style: ButtonStyle(
+                              foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[2]),
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 3)
+                                  )
+                              )
+                          ),
+                          onPressed: () => null
                       ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      })
+                      SizedBox(
+                        width: 5,
+                      ),
+                      ElevatedButton(
+                          child: Text(
+                              "0:10".toUpperCase(),
+                              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)
+                          ),
+                          style: ButtonStyle(
+                              foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[0]),
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 3)
+                                  )
+                              )
+                          ),
+                          onPressed: () => null
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      ElevatedButton(
+                          child: Text(
+                              "0:10".toUpperCase(),
+                              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)
+                          ),
+                          style: ButtonStyle(
+                              foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[1]),
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 3)
+                                  )
+                              )
+                          ),
+                          onPressed: () => null
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                    ],
+                  ),
+
                 ],
               ),
             ),
@@ -140,12 +175,12 @@ class _TimerSimpleState extends State<TimerSimple>
               alignment: Alignment.bottomCenter,
               children: [
                 Container(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                   child: ClipRRect(
                     child: BackdropFilter(
                       filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
                       child: Container(
-                        margin: EdgeInsets.only(bottom: 50),
+                        margin: EdgeInsets.only(bottom: 0),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.grey[800]!.withOpacity(0.5),
@@ -221,10 +256,8 @@ class _TimerSimpleState extends State<TimerSimple>
                                         ),
                                       ),
                                       Text(
-                                        'RUNDEN',
-                                        style: TextStyle(
-                                            fontSize: 28.0,
-                                            color: Colors.white),
+                                          'Runden',
+                                          style: TextStyle(fontSize: 38.0)
                                       ),
                                       Row(
                                         mainAxisAlignment: MainAxisAlignment.center,
@@ -248,7 +281,7 @@ class _TimerSimpleState extends State<TimerSimple>
                                                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                                                       RoundedRectangleBorder(
                                                           borderRadius: BorderRadius.circular(18.0),
-                                                          side: BorderSide(color: timerTypeColor[timerType], width: 5)
+                                                          side: BorderSide(color: timerTypeColorR[timerType], width: 5)
                                                       )
                                                   )
                                               ),
@@ -269,7 +302,7 @@ class _TimerSimpleState extends State<TimerSimple>
                                                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                                                       RoundedRectangleBorder(
                                                           borderRadius: BorderRadius.circular(18.0),
-                                                          side: BorderSide(color: timerTypeColor[timerType], width: 5)
+                                                          side: BorderSide(color: timerTypeColorR[timerType], width: 5)
                                                       )
                                                   )
                                               ),
@@ -352,9 +385,6 @@ class _TimerSimpleState extends State<TimerSimple>
                                                 );
                                               }),
                                         ],),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
                                     ],
                                   ),
                                 ),
@@ -365,62 +395,7 @@ class _TimerSimpleState extends State<TimerSimple>
                       ),
                     ),
                   ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                        child: Text(
-                            "0:05".toUpperCase(),
-                            style: TextStyle(fontSize: 38.0, fontWeight: FontWeight.bold)
-                        ),
-                        style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[2]),
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 5)
-                                )
-                            )
-                        ),
-                        onPressed: () => null
-                    ),
-                    ElevatedButton(
-                        child: Text(
-                            "0:10".toUpperCase(),
-                            style: TextStyle(fontSize: 38.0, fontWeight: FontWeight.bold)
-                        ),
-                        style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[0]),
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 5)
-                                )
-                            )
-                        ),
-                        onPressed: () => null
-                    ),
-                    ElevatedButton(
-                        child: Text(
-                            "0:10".toUpperCase(),
-                            style: TextStyle(fontSize: 38.0, fontWeight: FontWeight.bold)
-                        ),
-                        style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all<Color>(timerTypeColorBg[1]),
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[800]!.withOpacity(0.5)),
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.grey[800]!.withOpacity(0.5), width: 5)
-                                )
-                            )
-                        ),
-                        onPressed: () => null
-                    ),
-                    ],
-                ),
+
                 ],
               ),
             ),
--- lib/Theme/colors.dart
+++ lib/Theme/colors.dart
@@ -12,9 +12,19 @@ Color titleColor = Color(0xff3D3D3D);
 Color containerBg = Color(0xff161A1F);
 Color scaffoldBg = Color(0xff161A1F);
 
-Color timerColorPause = Color(0xff4CAF50);
+Color timerColorPause = Color(0xff4caf50);
 Color timerColorPauseBg = Color(0xff1B5E20);
+Color timerColorPauseR = Color(0xff225027);
+
 Color timerColorFight = Color(0xffdc2020);
 Color timerColorFightBg = Color(0xff831313);
-Color timerColorPrep = Color(0xffFFEB3B);
-Color timerColorPrepBg = Color(0xff897e1f);
+Color timerColorFightR = Color(0xff650000);
+
+Color timerColorPrep2 = Color(0xffFFEB3B);
+Color timerColorPrep2Bg = Color(0xff897e1f);
+Color timerColorPrep2R = Color(0xff756B1C);
+
+Color timerColorPrep = Color(0xff4c87af);
+Color timerColorPrepBg = Color(0xff1b5eaf);
+Color timerColorPrepR = Color(0xff223D50);
+
